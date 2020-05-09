761ba3b352309b7a97f7189837aabda6961bca40
Vlad <vladikiy@gmail.com>
2020-05-10 00:26:20 +0300

    more tests for SleepAlarmViewModelImp

--- PlaySleepRecordAlarm/PlaySleepRecordAlarmTests/SleepAlarmViewModelTests.swift
+++ PlaySleepRecordAlarm/PlaySleepRecordAlarmTests/SleepAlarmViewModelTests.swift
@@ -30,6 +30,9 @@ class SleepAlarmViewModelTests: XCTestCase {
         localNotificationController = LocalNotificationControllerMock()
         presenter = SleepAlarmPresenterMock()
         
+        audioRecorderController.permissionWasGranted = true
+        localNotificationController.permissionWasGranted = true
+        
         sleepAlarmViewModel = SleepAlarmViewModelImp(
             audioPlayerController: audioPlayerController,
             alarmAudioPlayerController: alarmAudioPlayerController,
@@ -40,6 +43,7 @@ class SleepAlarmViewModelTests: XCTestCase {
     }
 
     override func tearDown() {
+        sleepAlarmViewModel.shouldReloadStateView(nil)
     }
     
     // MARK:- Tests
@@ -52,5 +56,100 @@ class SleepAlarmViewModelTests: XCTestCase {
         sleepAlarmViewModel.play()
         
         XCTAssertFalse(sleepAlarmViewModel.isRunnning)
+    }
+    
+    func testPermissionsGranted() {
+        audioRecorderController.permissionWasGranted = true
+        localNotificationController.permissionWasGranted = true
+        
+        sleepAlarmViewModel.requestPermissions()
+        sleepAlarmViewModel.play()
+        
+        XCTAssertTrue(sleepAlarmViewModel.isRunnning)
+    }
+    
+    func testSleepAudioIsPlaying() {
+        sleepAlarmViewModel.requestPermissions()
+        
+        presenter.selectedSleepTimer = .seconds(10)
+        sleepAlarmViewModel.rows[0].handleSelection(at: IndexPath())
+        
+        sleepAlarmViewModel.play()
+        XCTAssertTrue(audioPlayerController.isPlaying)
+    }
+    
+    func testSleepAudioIsPaused() {
+        sleepAlarmViewModel.requestPermissions()
+        
+        presenter.selectedSleepTimer = .seconds(10)
+        sleepAlarmViewModel.rows[0].handleSelection(at: IndexPath())
+        
+        sleepAlarmViewModel.play()
+        sleepAlarmViewModel.pause()
+        
+        XCTAssertFalse(audioPlayerController.isPlaying)
+    }
+    
+    func testFallingAsleepViewState() {
+        let fallingAsleepExpectation = expectation(description: "Switch to FallingAsleep state")
+        
+        sleepAlarmViewModel.requestPermissions()
+        
+        presenter.selectedSleepTimer = .seconds(10)
+        sleepAlarmViewModel.rows[0].handleSelection(at: IndexPath())
+        
+        sleepAlarmViewModel.shouldReloadStateView { stateView in
+            if stateView == .fallingAsleep {
+                fallingAsleepExpectation.fulfill()
+            } else {
+                XCTAssertTrue(false)
+            }
+        }
+        
+        sleepAlarmViewModel.play()
+        
+        wait(for: [fallingAsleepExpectation], timeout: 10)
+    }
+    
+    func testSleepTimerSetToOff_fastSwitchToFallingAsleepViewState() {
+        let sleepingExpectation = expectation(description: "Switch to Sleeping state")
+        sleepingExpectation.expectedFulfillmentCount = 2
+        sleepingExpectation.assertForOverFulfill = true
+        
+        sleepAlarmViewModel.requestPermissions()
+        
+        presenter.selectedSleepTimer = .off
+        sleepAlarmViewModel.rows[0].handleSelection(at: IndexPath())
+        
+        sleepAlarmViewModel.shouldReloadStateView { stateView in
+            if stateView == .sleeping || stateView == .fallingAsleep {
+                sleepingExpectation.fulfill()
+            } else {
+                XCTAssertTrue(false)
+            }
+        }
+        
+        sleepAlarmViewModel.play()
+        
+        wait(for: [sleepingExpectation], timeout: 3)
+    }
+    
+    func testAudioIsRecording() {
+        let isRecordingExpectation = expectation(description: "Recording")
+        
+        sleepAlarmViewModel.requestPermissions()
+        
+        presenter.selectedSleepTimer = .seconds(1)
+        sleepAlarmViewModel.rows[0].handleSelection(at: IndexPath())
+        
+        sleepAlarmViewModel.shouldReloadStateView { [unowned self] stateView in
+            if stateView == .sleeping && self.audioRecorderController.isRecording {
+                isRecordingExpectation.fulfill()
+            }
+        }
+        
+        sleepAlarmViewModel.play()
+        
+        wait(for: [isRecordingExpectation], timeout: 3)
     }
 }
